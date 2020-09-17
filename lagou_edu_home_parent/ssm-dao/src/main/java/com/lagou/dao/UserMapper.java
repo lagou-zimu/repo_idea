dbeac8c7039806b7065865c9ee03ca091a1f3c9b
lagou-zimu <sunzhichao0713@163.com>
2020-09-17 16:34:46 +0800

    Update UserMapper.java

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -54,7 +54,13 @@ public interface UserMapper {
 
 
 
-
+    public void test11();
+    public void test21();
+    public void test31();
+    public void test41();
+    public void test51();
+    public void test61();
+    public void test71();
 
 
 
